f3a9342d6f244590fb6193aefcf9dc778e44899a
okotaku <to78314910@gmail.com>
2023-05-06 18:18:33 +0900

    transformers v4.28.1

--- ensemble_transformers/models/ensemble_ocr.py
+++ ensemble_transformers/models/ensemble_ocr.py
@@ -87,6 +87,8 @@ class EnsembleForVisionEncoderDecoderModel(EnsembleGenerationMixin,
         *args,
         **kwargs,
     ):
+        if 'past_key_values' in kwargs and kwargs['past_key_values'][0] is None:
+            kwargs['past_key_values'] = None
         return self.models[0].prepare_inputs_for_generation(*args, **kwargs)
 
     def _reorder_cache(self, past, beam_idx):
